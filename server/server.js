f4afc68e746505198c5d66807b20d544fc64bbd8
ahumble000 <ahumble882@gmail.com>
2025-08-12 02:06:08 +0500

    v-01

--- server/server.js
+++ server/server.js
@@ -16,8 +16,7 @@ const server = http.createServer(app);
 
 // Configure CORS
 const corsOptions = {
-  origin: 'https://notify-alpha-nine.vercel.app',
-  credentials: true,
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
